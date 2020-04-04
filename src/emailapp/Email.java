22475376c414a8cd0873f1d24224f7b248e33a47
MKA1500 <maciejkabsch@gmail.com>
2020-04-04 19:02:09 +0200

    first methods

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -14,9 +16,27 @@ public class Email {
 		this.lastName = lastName;
 		
 		System.out.println("Email created: " + this.firstName + " " + this.lastName);
+		
+		// call a method asking for the department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
 	}
 	
 	// ask for the department
+	private String setDepartment() {
+		System.out.println("Department codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nType department:");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) { 
+			return "sales"; 
+		} else if (depChoice == 2) {
+			return "dev";
+		} else if (depChoice == 3) {
+			return "acc";
+		} else {
+			return "";
+		}
+	}
 	
 	// generate a random password
 	
