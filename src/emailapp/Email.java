fe67f5c1e0662a6daf7dfad2accf2c6f41f99c47
MKA1500 <maciejkabsch@gmail.com>
2020-04-19 13:46:43 +0200

    generate email

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,15 +7,18 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private int mailboxCapacity;
 	private int defaultPasswordLength;
 	private String alternateEmail;
+	private String companyPrefix;
 	
 	// constructor to receive the first name and last name
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.defaultPasswordLength = 8;
+		this.companyPrefix = "audi.eu";
 		
 		System.out.println("Email created: " + this.firstName + " " + this.lastName);
 		
@@ -27,11 +30,23 @@ public class Email {
 		this.password = generateRandomPassword(defaultPasswordLength);
 		
 		System.out.println("\nYour password is " + this.password);
+		
+		// combine elements to generate e-mail
+		this.email = this.firstName.toLowerCase() + 
+				"." + this.lastName.toLowerCase() + 
+				"@" + this.department + "." + this.companyPrefix;
+		
+		System.out.println("\nYour email is " + this.email);
 	}
 	
 	// ask for the department
 	private String setDepartment() {
-		System.out.println("Department codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nType department:");
+		System.out.println("Department codes:"
+				+ "\n1 for Sales"
+				+ "\n2 for Development"
+				+ "\n3 for Accounting"
+				+ "\n0 for none"
+				+ "\nType department:");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) { 
