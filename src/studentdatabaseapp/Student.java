49922e854934a724e9078ee856f4ab55b6ea835e
MKA1500 <maciejkabsch@gmail.com>
2020-05-02 17:51:49 +0200

    basic student setup

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,11 +1,30 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 	private String firstName;
 	private String lastName;
-	private String gradeYear;
+	private int gradeYear;
+	private int studentID;
+	private String courses;
+	private int tuitionBalance;
+	private int costOfCourse = 600;
 	
 	// Constructor: prompt user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Junior, \n2 - Mid Level, \n3 - Senior \nEnter student class level: ");
+		this.gradeYear = in.nextInt();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear);
+	}
 	
 	// Generate ID
 	
