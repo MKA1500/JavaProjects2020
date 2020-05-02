4587e667bc32cec5981fcb6afff508c95f16863c
MKA1500 <maciejkabsch@gmail.com>
2020-05-02 17:13:50 +0200

    new app

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,20 @@
+package studentdatabaseapp;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	
+	// Constructor: prompt user to enter student's name and year
+	
+	// Generate ID
+	
+	// Enroll in courses
+	
+	// View balance
+	
+	// Pay tuition
+	
+	// Show status
+	
+}
